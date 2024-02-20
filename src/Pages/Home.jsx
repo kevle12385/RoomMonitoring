c04f423500a9917895bca27d88ba96e30281d066
Kevin_Le <Kevle12385@ymail.com>
2024-02-19 23:12:18 -0800

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,10 +6,14 @@ function Home() {
   return (
     <>
 
-    <h1>Home</h1>
-    <h2> </h2>
-   
-    
+    <h1>Home </h1>
+    <h2>Created by Kevin Le</h2>
+    <h3>Note: The simulation graph does not update the real database 
+    {<br/>}because it crashes it. Keep in mind when using this application, 
+    {<br/>}creating, deleting, updating the rooms and alerts will actaully 
+    {<br/>}make changes to the database... </h3>
+ 
+  
     </>
     
   )
