89709c217cb9ee378115e825a9dc5381a64403fe
Kevin_Le <Kevle12385@ymail.com>
2024-02-24 16:21:07 -0800

    add link demo

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,7 +13,8 @@ function Home() {
     {<br/>}creating, deleting, updating the rooms and alerts will actaully 
     {<br/>}make changes to the database. If the buttons don't work, click on
     {<br/>}this link </h3>
- 
+    <a href="https://vimeo.com/916345917?share=copy" target="_blank">Watch the Demo</a>
+
   
     </>
     
